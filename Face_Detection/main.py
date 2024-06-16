e8f49eef6f47790faeae087539688a368e7dfd06
UriPomer <3051068921@qq.com>
2024-06-16 19:50:00 +0800

    initial face detection

---
+++ Face_Detection/main.py
@@ -0,0 +1,31 @@
+import cv2
+import matplotlib.pyplot as plt
+
+face_classifier = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+
+def detect_bounding_box(image):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 4)
+    return faces
+
+
+def main():
+    imagePath = "input_image.jpg"
+    img = cv2.imread(imagePath)
+
+    faces = detect_bounding_box(img)
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.figure(figsize=(20,10))
+    plt.imshow(img_rgb)
+    plt.axis('off')
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
